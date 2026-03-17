653d65e6623b6189ed6c720c7adfa95d702e3fee
rushiachini <tharunrushi@gmail.com>
2026-03-17 14:12:33 +0530

    UC4

---
+++ TestString2.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class TestString{
+public static void main(String[] args){
+Scanner input = new Scanner(System.in);
+
+if(args.length > 0){
+System.out.println("Greetings");
+
+}else{
+System.out.println("World");
+}
+}
+}
